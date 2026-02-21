5d4fe571e8ae3aa0aa6589767a59989a33e7aed3
Jason Nam <contact@jasonnam.com>
2026-02-21 18:16:40 +0700

    Update version to 0.3.0

--- Formula/flint.rb
+++ Formula/flint.rb
@@ -1,14 +1,10 @@
 class Flint < Formula
     desc "Generate projects and files from template."
-    homepage "https://github.com/flintbox/Flint"
-    url "https://github.com/flintbox/Flint/archive/0.2.0.tar.gz"
-    sha256 "213165e4346fe2912b1edc95f11fc527af2188608c8cfc7f24238ccc3dfab255"
+    homepage "https://github.com/flint-engine/flint"
+    url "https://github.com/flintbox/Flint/archive/0.3.0.tar.gz"
+    sha256 "7db82df395fc04f88ba2256ebb0e4f7566f2808a9a6e632a487867c2b58bdd8f"
 
     def install
-        # fixes an issue an issue in homebrew when both Xcode 9.3+ and command line tools are installed
-        # see more details here https://github.com/Homebrew/brew/pull/4147
-        # remove this once homebrew is patched
-        ENV["CC"] = Utils.popen_read("xcrun -find clang").chomp
         system "make", "release"
         bin.install ".build/release/flint"
     end
